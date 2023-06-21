5f0334b6cf84c4a80c28c821e697cb5640a7629c
spetrovi <spetrovi@redhat.com>
2023-06-21 12:40:26 +0200

    Adding support for local secret_key storing
    Signed-off-by: spetrovi <spetrovi@redhat.com>

--- spevnik/settings.py
+++ spevnik/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-SECRET_KEY = os.environ["SECRET_KEY"]
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
