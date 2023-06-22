40651ec832d41ef50967e06457db2c5681d8c279
spetrovi <spetrovi@redhat.com>
2023-06-22 15:14:38 +0200

    Getting rid of load_dotenv
    Signed-off-by: spetrovi <spetrovi@redhat.com>

--- spevnik/settings.py
+++ spevnik/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 
 from pathlib import Path
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "False") != "False"
