2d375b098abea1240f9532e788175796c09f9338
spetrovi <spetrovi@redhat.com>
2023-06-23 12:42:37 +0200

    Starting the basic logic with forms and models
    Signed-off-by: spetrovi <spetrovi@redhat.com>

--- spevnik_form/models.py
+++ spevnik_form/models.py
@@ -2,12 +2,24 @@ from django.db import models
 
 
 class Song(models.Model):
-    title = models.CharField(max_length=20)
-    pub_date = models.DateTimeField("date published")
+    def __str__(self):
+        metadata = models.OneToOneField(Metadata, on_delete=models.CASCADE)
+        stanzas = models.ManyToManyField(Stanza)
+        return f"Song {self.metadata.title}"
+
+
+class Stanza(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"Stanza {self.id}"
+
+
+class Verse(models.Model):
+    stanza = models.ForeignKey(Stanza, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.title
-
+        return self.text
 
 class Author(models.Model):
     name = models.CharField(max_length=15)
@@ -16,3 +28,16 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name + " " + self.surname
+        
+class Metadata(models.Model):
+    title = models.CharField(max_length=200)
+    gathered_date = models.DateTimeField("date gathered")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    rhytm = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    
+    song = models.OneToOneField(Song, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"Metadata {self.id}"
+
+
